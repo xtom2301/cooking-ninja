ce97244a2ea941e76e19dd170a0154294ad32382
tmartin <xtom2301@gmail.com>
2022-01-29 20:44:30 +0100

    darkMode

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ import Recipe from './pages/recipe/Recipe';
 import Search from './pages/search/Search';
 import Navbar from './components/Navbar';
 import ThemeSelector from './components/ThemeSelector';
+import { useTheme } from './hooks/useTheme';
 
 function App() {
+  const { mode } = useTheme();
+
   return (
-    <div className='App'>
+    <div className={`App ${mode}`}>
       <Router>
         <Navbar />
         <ThemeSelector />
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,14 +1,16 @@
 import './Recipe.css';
 import { useParams } from 'react-router-dom';
 import { useFetch } from '../../hooks/useFetch';
+import { useTheme } from '../../hooks/useTheme';
 
 const Recipe = () => {
   const { id } = useParams();
   const url = 'http://localhost:3000/recipes/' + id;
   const { data: recipe, isPending } = useFetch(url);
+  const { mode } = useTheme();
 
   return (
-    <div className='recipe'>
+    <div className={`recipe ${mode}`}>
       {isPending && <p className='loading'>Loading...</p>}
       {recipe && (
         <>
