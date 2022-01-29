6bd9cfbe8a239c30a8d04fc8cf8797a4d3f35fb1
tmartin <xtom2301@gmail.com>
2022-01-29 19:22:01 +0100

    Search

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,7 +1,22 @@
+import { useLocation } from 'react-router-dom';
 import './Search.css';
+import { useFetch } from '../../hooks/useFetch';
+import RecipeList from '../../components/RecipeList';
 
 const Search = () => {
-  return <div>Search</div>;
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get('q');
+  const url = 'http://localhost:3000/recipes?q=' + query;
+  const { isPending, data: recipes } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className='page-title'>Recipes including "{query}"</h2>
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipes && <RecipeList recipes={recipes} />}
+    </div>
+  );
 };
 
 export default Search;
