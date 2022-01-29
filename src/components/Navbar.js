2c5740b31f9a4daeafac8e90bb6da658d553f318
tmartin <xtom2301@gmail.com>
2022-01-29 19:47:38 +0100

    context api

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import Searchbar from './Searchbar';
+import { useTheme } from '../hooks/useTheme';
 
 const Navbar = () => {
+  const { color } = useTheme();
   return (
-    <div className='navbar'>
+    <div className='navbar' style={{ background: color }}>
       <nav>
         <Link to='/' className='brand'>
           <h1>Cooking Ninja</h1>
