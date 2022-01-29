a19688fbfb284ee52489b85451d5c8fb2b208b10
tmartin <xtom2301@gmail.com>
2022-01-29 20:08:22 +0100

    usereducer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import Searchbar from './Searchbar';
 import { useTheme } from '../hooks/useTheme';
 
 const Navbar = () => {
-  const { color } = useTheme();
+  const { color, changeColor } = useTheme();
   return (
     <div className='navbar' style={{ background: color }}>
-      <nav>
+      <nav onClick={() => changeColor('gray')}>
         <Link to='/' className='brand'>
           <h1>Cooking Ninja</h1>
         </Link>
